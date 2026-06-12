4fca03b6b10754442d68d9864c9c716a708e9b4c
aoi-3125 <aoi.yamauchi@tcs.com>
2026-06-12 11:47:01 +0900

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,30 +1,27 @@
-name: Python application
+from src.main import add
+import pytest
 
-on:
-  push:
-    branches:
-      - main
-
-jobs:
-  test:
-    runs-on: ubuntu-latest
-
-    steps:
-      - name: Check out repository
-        uses: actions/checkout@v3
-
-      - name: Set up Python
-        uses: actions/setup-python@v4
-        with:
-          python-version: '3.12'
-
-      - name: Install dependencies
-        run: |
-          python -m pip install --upgrade pip
-          if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
-          pip install pytest
-
-      - name: Run tests
-        env:
-          PYTHONPATH: ${{ github.workspace }}
-        run: pytest
+def test_add():
+ """
+  assert add(7, 2) == 
+  assert add(5, "2", 3) == 
+  assert add("2", "8.2") == 
+  assert add("8", "1", 3.6) == 
+  assert add("1", "2") == 
+  assert add("3", "4", "5") == 
+  assert add(2.6, "4", None) == 
+  assert add(2.5, 3, "5.1") == 
+  assert add(5.4, 9.8) == 
+  assert add("1.4", "2", 3) == 
+  assert add(2.5, 3, "x") == 
+  assert add(2.1, 9.3, 3.7) == 
+  assert add(1, 6, 3) == 
+  assert add(1.6, 2.1) == 
+  assert add(3.1, 2) == 
+  assert add(1.9, 4.3, "2.6") == 
+  assert add("1", "2", 9) == 
+  assert add("4", "2.4", 1) == 
+  assert add(None, 3, "5.1") == 
+  assert add("b", 4, 5) == 
+"""
+    return
